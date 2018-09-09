0cbd6757e30dbc78a1a29ba454c80c1a501bdbf9
kiriltaran <kiriltaran@gmail.com>
2018-09-09 14:55:44 +0300

    done

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -1,67 +1,108 @@
 <template>
     <div class="todo">
         <h1 class="title">Checklist</h1>
-        <ul class="tasks">
-            <li v-for="task in tasks" :class="{complete : task.complete}">
-                <label>
-                    <input type="checkbox" v-model="task.complete" />
-                    {{task.name}}
-                </label>
-            </li>
-        </ul>
-        <div>
-            <ui-textbox placeholder="e.g. 'read vue.js guide'" v-model="newTaskName"></ui-textbox>
+        <ui-tabs fullwidth>
+            <ui-tab title="Pending">
+                <todo-list :tasks="tasks" :complete="false"></todo-list>
+            </ui-tab>
+            <ui-tab title="Complete">
+                <todo-list :tasks="tasks" :complete="true"></todo-list>
+            </ui-tab>
+        </ui-tabs>
+        <div class="actions">
+            <ui-textbox placeholder="e.g. 'read vue.js guide'" v-model="newTaskName" class="actions-input" @keyup.native.enter="addTask"></ui-textbox>
             <ui-button color="primary" @click="addTask" icon="add">Add</ui-button>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        data () {
-            return {
-                newTaskName : '',
-                tasks : [
-                    {name : 'create skeleton of todo', complete : true},
-                    {name : 'add ability to add tasks', complete : true},
-                    {name : 'clear task name after clicking "Add"', complete : false},
-                    {name : 'put "Add" button in one line with input', complete : false},
-                    {name : 'add new task by hitting Enter instead of clicking "Add"', complete : false},
-                    {name : 'replace <input> with <ui-checkbox> in tasks list', complete : false},
-                    {name : 'when task is complete cross it out', complete : false},
-                    {name : 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete : false},
-                    {name : 'don\'t allow to add empty tasks', complete : false},
-                    {name : 'make list of tasks scrollable, if there\'re are a lot of tasks', complete : false},
-                    {name : 'extract list item into a separate vue.js component', complete : false},
-                    {name : 'persist tasks list in a local storage', complete : false},
-                    {name : 'add animation on task completion', complete : false},
-                ]
-            }
-        },
+import todoList from "./todoList.vue";
 
-        methods : {
-            addTask () {
-                this.tasks.push({name : this.newTaskName, complete : false});
-            }
-        }
+export default {
+  components: {
+    todoList
+  },
+  data() {
+    return {
+      newTaskName: "",
+      tasks: [
+        { name: "create skeleton of todo", complete: true },
+        { name: "add ability to add tasks", complete: true },
+        { name: 'clear task name after clicking "Add"', complete: false },
+        { name: 'put "Add" button in one line with input', complete: false },
+        {
+          name: 'add new task by hitting Enter instead of clicking "Add"',
+          complete: true
+        },
+        {
+          name: "replace <input> with <ui-checkbox> in tasks list",
+          complete: false
+        },
+        { name: "when task is complete cross it out", complete: false },
+        {
+          name:
+            'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>',
+          complete: false
+        },
+        { name: "don't allow to add empty tasks", complete: false },
+        {
+          name: "make list of tasks scrollable, if there're are a lot of tasks",
+          complete: false
+        },
+        {
+          name: "extract list item into a separate vue.js component",
+          complete: false
+        },
+        { name: "persist tasks list in a local storage", complete: false },
+        { name: "add animation on task completion", complete: false }
+      ]
     };
+  },
+  mounted() {
+    this.getTasksFromStorage();
+  },
+  methods: {
+    addTask() {
+      if (this.newTaskName.trim() !== "") {
+        this.tasks.push({ name: this.newTaskName, complete: false });
+        this.setTasksToStorage();
+        this.newTaskName = "";
+      }
+    },
+    setTasksToStorage() {
+      const tasks = JSON.stringify(this.tasks);
+      localStorage.setItem("tasks", tasks);
+    },
+    getTasksFromStorage() {
+      if (localStorage.getItem("tasks")) {
+        this.tasks = JSON.parse(localStorage.getItem("tasks"));
+      }
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    .todo {
-        margin: auto;
-        background: #fff;
-        padding: 20px;
-        border-radius: 5px;
-        box-shadow: rgba(0, 0, 0, 0.3) 3px 3px 15px;
+.todo {
+  margin: auto;
+  background: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.3) 3px 3px 15px;
 
-        .title {
-            margin-top: 0;
-        }
+  .title {
+    margin-top: 0;
+  }
 
-        .tasks {
-            list-style: none;
-            padding: 0;
-        }
+  .actions {
+    display: flex;
+    justify-content: space-between;
+
+    &-input {
+      flex-grow: 1;
+      padding-right: 15px;
     }
+  }
+}
 </style>
---
+++ src/components/todoListItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <label :class="{complete : task.complete}">
+        <ui-checkbox v-model="task.complete">{{ task.name }}</ui-checkbox>
+    </label>
+</template>
+
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.complete {
+  text-decoration: line-through;
+}
+</style>
+
+
