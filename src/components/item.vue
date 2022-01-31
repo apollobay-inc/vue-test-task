19f6f8d07bbc6e4203fc2f1d1c0cfb76100411c1
WIllsnSmith <silverflash150@gmail.com>
2022-02-01 01:08:55 +0300

    some changing

---
+++ src/components/item.vue
@@ -0,0 +1,35 @@
+<template>
+  <label>
+    <ui-checkbox
+      :checked="isChecked"
+      @change="onChange"
+      value=""
+    >
+      {{ text }}
+    </ui-checkbox>
+  </label>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  model: {
+    event: "change",
+    prop: "isChecked",
+  },
+  props: {
+    text: String,
+    isChecked: Boolean,
+  },
+  methods: {
+    onChange (event) {
+     this.$emit('change', event)
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/components/todo.vue
+++ src/components/todo.vue
@@ -1,67 +1,142 @@
 <template>
-    <div class="todo">
-        <h1 class="title">Checklist</h1>
-        <ul class="tasks">
-            <li v-for="task in tasks" :class="{complete : task.complete}">
-                <label>
-                    <input type="checkbox" v-model="task.complete" />
-                    {{task.name}}
-                </label>
+  <div class="todo">
+    <h1 class="title">Checklist</h1>
+    <ui-tabs backgroundColor="clear">
+      <ui-tab :key="tab.title" :title="tab.title" v-for="tab in stateTabs">
+        <div v-if="tab.title === 'Pending'">
+          <ul class="tasks">
+            <li
+              v-for="(task, index) in tasks"
+              :key="index"
+              :class="{ complete: task.complete }"
+            >
+              <div v-if="!task.complete">
+                <item
+                  :text="task.name"
+                  :isChecked="task.complete"
+                  v-model="task.complete"
+                ></item>
+              </div>
             </li>
-        </ul>
-        <div>
-            <ui-textbox placeholder="e.g. 'read vue.js guide'" v-model="newTaskName"></ui-textbox>
-            <ui-button color="primary" @click="addTask" icon="add">Add</ui-button>
+          </ul>
         </div>
+        <div v-if="tab.title === 'Completed'">
+          <ul class="tasks">
+            <li
+              v-for="(task, index) in tasks"
+              :key="index"
+              :class="{ complete: task.complete }"
+            >
+              <div v-if="task.complete">
+                <item
+                  :text="task.name"
+                  :isChecked="task.complete"
+                  v-model="task.complete"
+                ></item>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </ui-tab>
+    </ui-tabs>
+
+    <div class="add">
+      <ui-textbox
+        class="textbox"
+        maxlength="100"
+        placeholder="e.g. 'read vue.js guide'"
+        v-model="newTaskName"
+        @keydown-enter="addTask"
+      ></ui-textbox>
+      <ui-button
+        color="primary"
+        :disabled="isAdd"
+        @click="addTask"
+        class="button"
+        >Add item</ui-button
+      >
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data () {
-            return {
-                newTaskName : '',
-                tasks : [
-                    {name : 'create skeleton of todo', complete : true},
-                    {name : 'add ability to add tasks', complete : true},
-                    {name : 'clear task name after clicking "Add"', complete : false},
-                    {name : 'put "Add" button in one line with input', complete : false},
-                    {name : 'add new task by hitting Enter instead of clicking "Add"', complete : false},
-                    {name : 'replace <input> with <ui-checkbox> in tasks list', complete : false},
-                    {name : 'when task is complete cross it out', complete : false},
-                    {name : 'split tasks into "pending" and "complete" tabs using keen-ui component <ui-tabs>', complete : false},
-                    {name : 'don\'t allow to add empty tasks', complete : false},
-                    {name : 'make list of tasks scrollable, if there\'re are a lot of tasks', complete : false},
-                    {name : 'extract list item into a separate vue.js component', complete : false},
-                    {name : 'persist tasks list in a local storage', complete : false},
-                    {name : 'add animation on task completion', complete : false},
-                ]
-            }
-        },
-
-        methods : {
-            addTask () {
-                this.tasks.push({name : this.newTaskName, complete : false});
-            }
-        }
+import item from "../components/item.vue";
+const TODO_ITEMS = "todoItems";
+export default {
+  components: { item },
+  data() {
+    return {
+      newTaskName: "",
+      tasks: [],
+      isAdd: true,
+      stateTabs: [{ title: "Pending" }, { title: "Completed" }],
     };
+  },
+  created: function () {
+    const storedTasks = localStorage.getItem(TODO_ITEMS);
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+    }
+  },
+  watch: {
+    tasks: {
+      handler: function () {
+        localStorage.setItem(TODO_ITEMS, JSON.stringify(this.tasks));
+      },
+      deep: true,
+    },
+    newTaskName: function (val) {
+      if (val.length > 0) {
+        this.isAdd = false;
+      } else {
+        this.isAdd = true;
+      }
+    },
+  },
+  methods: {
+    addTask() {
+      if (this.newTaskName.length > 0) {
+        this.tasks.push({ name: this.newTaskName, complete: false });
+        const lastTask = this.tasks[this.tasks.length - 1];
+        localStorage.setItem(this.tasks.length - 1, JSON.stringify(lastTask));
+        this.newTaskName = "";
+      }
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-    .todo {
-        margin: auto;
-        background: #fff;
-        padding: 20px;
-        border-radius: 5px;
-        box-shadow: rgba(0, 0, 0, 0.3) 3px 3px 15px;
+.todo {
+  margin: auto;
+  background: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.3) 3px 3px 15px;
 
-        .title {
-            margin-top: 0;
-        }
+  .title {
+    margin-top: 0;
+  }
 
-        .tasks {
-            list-style: none;
-            padding: 0;
-        }
+  .tasks {
+    list-style: none;
+    padding: 0;
+    overflow: auto;
+    height: 300px;
+    width: 500px;
+  }
+  .complete {
+    text-decoration: line-through;
+  }
+  .add {
+    display: flex;
+    justify-content: space-between;
+    .textbox {
+      width: 350px;
     }
+    .button {
+      border-radius: 1rem;
+    }
+  }
+}
 </style>
